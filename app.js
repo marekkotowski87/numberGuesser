a16cbc45ebb41f3f5f499cab58580dc8c81f9c0b
Marek Kotowski <marek.kotowski87@gmail.com>
2018-12-05 23:22:04 +0100

    Update app.js
    Play again

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Game values
 let min = 1,
   max = 10,
-  winningNum = 2,
-  guessesLeft = 3;
+  winningNum = getRandomNum(min, max);
+guessesLeft = 3;
 
 // UI elements
-const game = document.querySelector('game'),
+const game = document.querySelector('#game'),
   minNum = document.querySelector('.min-num'),
   maxNum = document.querySelector('.max-num'),
   guessBtn = document.querySelector('#guess-btn'),
@@ -15,6 +15,14 @@ const game = document.querySelector('game'),
 //Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
+
+// Play again event listner
+game.addEventListener('mousedown', function(e) {
+  if (e.target.className === 'play-again') {
+    console.log(e.target.className);
+    window.location.reload();
+  }
+});
 
 // Listen for guess
 guessBtn.addEventListener('click', function() {
@@ -65,4 +73,15 @@ function gameOver(won, msg) {
   guessInput.style.borderColor = color;
   // Set message
   setMessage(msg, color);
+
+  // Play again
+  guessBtn.value = 'Play Again';
+  guessBtn.className += 'play-again';
+}
+
+// Get Winning Number
+function getRandomNum(min, max) {
+  var num = Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(`You've got me :) Winning number is ${num}`);
+  return num;
 }
